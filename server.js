c61d744d72c581ba50597873e38bc33f8ab1251e
Yash Garg <yash.yg.garg111@gmail.com>
2021-12-27 01:31:58 +0530

    second

--- server.js
+++ server.js
@@ -14,19 +14,17 @@ app.get('/', (req, res) => {
 app.get('/:room', (req, res) => {
     const roomId = req.params.room
     res.render('room', { roomId })
-    console.log('17joining the room:', roomId);
+    console.log('joining the room:', roomId);
 })
 
 io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
-        console.log('22 joining room', roomId);
-        console.log('user:', userId);
         socket.join(roomId)
-        socket.broadcast.to(roomId).emit('user-connected', userId)
+        socket.broadcast.emit('user-connected', userId)
 
-        // socket.on('disconnect', () => {
-        //     socket.broadcast.emit('user-disconnected', userId)
-        // })
+        socket.on('disconnect', () => {
+            socket.broadcast.emit('user-disconnected', userId)
+        })
     })
 })
 
